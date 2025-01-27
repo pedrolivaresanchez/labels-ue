3f1257a1feadcddef0dd89046cfe79e3d2bec444
Your Name <your_email@example.com>
2025-01-27 12:10:37 +0400

    Login and photo issues

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const nextConfig = {
         hostname: 'iegzztxbiaxfheqvmxrj.supabase.co',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
     ],
   },
 }
